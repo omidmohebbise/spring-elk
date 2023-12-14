cb5fe271a5f529db31879efc35751c1e66f3108d
omid mohebbi <omidmohebbise@gmail.com>
2023-12-14 10:09:25 +0100

    initial the project

---
+++ src/main/java/com/omidmohebbise/springelk/SpringelkApplication.java
@@ -0,0 +1,13 @@
+package com.omidmohebbise.springelk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringelkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringelkApplication.class, args);
+    }
+
+}
